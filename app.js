3158e52801c4c5db17ad344d30b2f7cce8dbafc9
Lin Chun <yichun03@gmail.com>
2022-03-22 00:14:03 +0800

    猜數字遊戲

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
+const data = {};
+
+function checkNumber(input) { 
+  var re = /^[0-9]*/g;//判斷字串是否為數字//判斷正整數/[1−9] [0−9]∗]∗/ 
+    if (!re.test(nubmer)) { 
+      return -1
+    } 
+    return parseInt(input)
+}
+
 // event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
@@ -32,13 +42,39 @@ function handleEvent(event) {
 
   client.getProfile(userId)
   .then((profile) => {
-    console.log(profile.displayName);
-    // create a echoing text message
-    const echo = { type: 'text', text: `${profile.displayName} say ${event.message.text} ` };
-  
-  
-    // use reply API
-    return client.replyMessage(event.replyToken, echo);
+    console.log(event);
+
+    if (event.message.text == "開始遊戲"){
+      data[event.source.groupId] = Math.floor(Math.random()*100);
+      const echo = { type: 'text', text: `歡迎 ${profile.displayName} 開起一個遊戲，請猜1-100` };
+      return client.replyMessage(event.replyToken, echo);
+    } 
+    else if(!data.hasOwnProperty(event.source.groupId)){
+      const echo = { type: 'text', text: `請先開局，輸入 '開始遊戲'` };
+      return client.replyMessage(event.replyToken, echo);
+    }
+    else if(data.hasOwnProperty(event.source.groupId)) {
+      // do check number
+      const num = parseInt(event.message.text);
+      console.log(num);
+      if (num < 0) {
+        const echo = { type: 'text', text: `請輸入數字` };
+        return client.replyMessage(event.replyToken, echo);
+      } else {
+
+        const text = num > data[event.source.groupId] ? '再小一點' : (num == data[event.source.groupId] ? '恭喜你猜對了' : '再大一點')
+        const echo = { type: 'text', text: text };
+        if (num == data[event.source.groupId])
+          delete data[event.source.groupId];
+        return client.replyMessage(event.replyToken, echo);
+      }
+    }else {
+      console.log(data)
+    }
+
+
+
+
   })
   .catch((err) => {
     // error handling
